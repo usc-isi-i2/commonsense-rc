f092028f995cacff56d372e4c4692861adeb0cd3
filievski <filip.dbrsk@gmail.com>
2020-03-28 17:43:53 -0700

    cleanup dir before running

--- run_mowgli_trian.sh
+++ run_mowgli_trian.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+rm data/*.log
+rm data/*vocab
+rm data/*.json
+rm checkpoint/*.mdl
+
 cfg="cfg/socialiqa-train-dev.yaml"
 for dataset in "socialiqa-train-dev" #"alphanli" #"physicaliqa-train-dev" # "socialiqa-train-dev"
 do
